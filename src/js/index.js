894467063d30a54e6110ede56c50481de2754290
h1jun <dittoavec@gmail.com>
2021-09-02 02:54:07 +0900

    Fix handleMovieClick function

--- src/js/index.js
+++ src/js/index.js
@@ -8,21 +8,6 @@ headerRender();
 const main = document.createElement('main');
 document.querySelector('#root').append(main);
 
-
 window.onload = async () => {
     await initialRoutes(main);
-    await test();
-}
-
-function test() {
-    const handleDetail = document.querySelectorAll(".movie-detail");
- 
-    handleDetail.forEach(element => {
-        element.addEventListener("click", (event) =>  {
-            const pathName = event.currentTarget.getAttribute("route")
-            const movieId = event.currentTarget.id;
-
-            setHistoryPath(pathName, main, movieId);
-        })
-    });
 }
--- src/js/router.js
+++ src/js/router.js
@@ -8,6 +8,7 @@ const routes = {
 
 const initialRoutes = async (el) => {
     await renderHTML(el, routes['/']);
+    handleMovieClick();
 }
 
 const setHistoryPath = (pathName, el, movieId) => {
@@ -23,13 +24,27 @@ const renderHTML = async (el, route) => {
 
 window.addEventListener('popstate', async (event) => {
     const mainTag = document.querySelector('main');
+
     if (location.pathname.split('/')[1] === "") {
-        renderHTML(mainTag, routes['/']);
+        await renderHTML(mainTag, routes['/']);
     } else if (location.pathname.split('/')[1] === "detail") {
-        renderHTML(mainTag, routes['/' + location.pathname.split('/')[1]]);
+        await renderHTML(mainTag, routes['/' + location.pathname.split('/')[1]]);
     }
     
+    handleMovieClick();
 });
 
+
+function handleMovieClick() {
+    const handleDetail = document.querySelectorAll(".movie-detail");
+    const main = document.querySelector('main');
+    handleDetail.forEach(element => {
+        element.addEventListener("click", async (event) => {
+            const pathName = event.currentTarget.getAttribute("route")
+            const movieId = event.currentTarget.id;
+            await setHistoryPath(pathName, main, movieId);
+        })
+    });
+}
 
 export { initialRoutes, setHistoryPath }
